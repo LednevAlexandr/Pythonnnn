07115b6c937cf123171abe3fd83405a23ed9519f
LednevAlexandr <92679120+LednevAlexandr@users.noreply.github.com>
2022-01-31 12:25:56 +0300

    Update Sem3-4_23.py

--- Sem3-4_23.py
+++ Sem3-4_23.py
@@ -2,13 +2,13 @@
 #Парой считаем первый и последний элемент,
 #второй и предпоследний и т.д.пример: [2,3,4,5,6,] => [12,15,16];[2,3,5,6]=>[12,15]
 import math 
-a = [2,5,5,7,6]
+a = [2,5,7,6]
 
 def sum_two(a):
   b=[]
   for i in range(len(a)):
-     if i >= math.ceil(len(a)/2):break 
-     b.append(a[i]*a[-i-1])
+     if i < len(a)/2:
+       b.append(a[i]*a[-i-1])
   return b
 
 print(sum_two(a))
