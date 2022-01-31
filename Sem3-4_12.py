72facb5c00ab64b013aa6f64f7687afa34b03eec
alex <you@example.com>
2022-01-31 21:55:59 +0300

    новые

--- Sem3-4_12.py
+++ Sem3-4_12.py
@@ -1,13 +1,7 @@
 #Для натурального числа N создать словарь индекс-значение,состоящих из элементов последовательности 3k+1
 
 def new_dict(n):
-    a={}
-    k=1
-    for i in range(0,n):
-        a[k]=3*k+1
-        k+=1
+    a={k:3*k+1 for k in range(1,n+1)}
     return a
 
-g=new_dict(10)
-print(g)
-print(g[7])
+print(new_dict(5))
