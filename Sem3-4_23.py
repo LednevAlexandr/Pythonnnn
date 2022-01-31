134fbca5f1a16efb0cb1409e12fbc5243cd99760
LednevAlexandr <92679120+LednevAlexandr@users.noreply.github.com>
2022-01-31 11:52:40 +0300

    Create Sem3-4_23.py

---
+++ Sem3-4_23.py
@@ -0,0 +1,18 @@
+#23 найти произведение пар чисел в списке.
+#Парой считаем первый и последний элемент,
+#второй и предпоследний и т.д.пример: [2,3,4,5,6,] => [12,15,16];[2,3,5,6]=>[12,15]
+import math 
+a = [2,3,5,7,6]
+
+def sum_two(a):
+  b=[]
+  t=1
+  for i in range(len(a)):
+     if i >= math.ceil(len(a)/2)  :break  
+     b.append(a[i]*a[-t])
+     t+=1
+  return b
+
+print(sum_two(a))
+print(len(a))
+print(math.ceil(len(a)/2))
