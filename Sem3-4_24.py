fe1e5ab23ca58a35809cc0e32829d2b72538b37f
LednevAlexandr <92679120+LednevAlexandr@users.noreply.github.com>
2022-01-31 14:47:02 +0300

    Update Sem3-4_24.py

--- Sem3-4_24.py
+++ Sem3-4_24.py
@@ -1,12 +1,13 @@
 #24 в заданном списке вещественных чисел найдите разницу между максимальным
 #и минимальным значением дробной части элементов. Пример [1.1,1.2,3.1,5,10.01]=>0.19
 from random import * 
-a = [3.4,5,54.34,5.3]
+a = [3.15,5.26,54.34,5.3]
 print(a)
 def max_min(a):
-  max=a[0]
-  for i in range(len(a)):
-    if a[i]>max: max = a[i]
-  return max
+  b = [i - int(i) for i in a]
+  max_n=max(b)
+  print(max_n)
+  min_n=min(b)
+  print(min_n)
+  return max_n-min_n
 print(max_min(a))    
-
