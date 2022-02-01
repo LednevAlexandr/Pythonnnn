9b9dfe3c80a96e0666ae0bd1ed15e92dbd932b0b
LednevAlexandr <92679120+LednevAlexandr@users.noreply.github.com>
2022-02-01 10:52:56 +0300

    Update Sem3-4_28.py

--- Sem3-4_28.py
+++ Sem3-4_28.py
@@ -4,11 +4,15 @@
 
 
 def f(a,b,c):
-    d = b**2 - 4*a*c
+  d = b**2 - 4*a*c
+  if d==0:
+     return -b/2*a
+  elif d>0:
     x1= (-b+(d**0.5))/2*a
     x2= (-b-(d**0.5))/2*a
     return x1,x2
+  else:
+     return -1
 
-print(f(1,-5,6))
-
-
+k=list(f(1,-5,6))
+print(type(k))
