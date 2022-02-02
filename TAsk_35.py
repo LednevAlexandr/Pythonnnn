e57cf16775e97d3dc4106cf72e65de567b5681c1
LednevAlexandr <92679120+LednevAlexandr@users.noreply.github.com>
2022-02-02 15:17:01 +0300

    Update TAsk_35.py

--- TAsk_35.py
+++ TAsk_35.py
@@ -1,15 +1,7 @@
 # Определить , присутствует ли в заданном масиве некоторое число
 
 import random
-def completion_array(array):
-  s=''
-  for i in range(len(array)):
-    array[i]=random.randint(-9,9)
-    s=s+str(array[i])+"  "
-  return s
-
-a = list(range(0,10))
+a = [random.randint(-9,9) for i in range(10)]
 b = int(input())
-print(completion_array(a))
-
+print(a)
 print(b in a)
