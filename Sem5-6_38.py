b99ed5baa463f44c87143e351e8750c7e952599b
LednevAlexandr <92679120+LednevAlexandr@users.noreply.github.com>
2022-02-04 09:12:53 +0300

    Update Sem5-6_38.py

--- Sem5-6_38.py
+++ Sem5-6_38.py
@@ -1,7 +1,6 @@
 #38 напишите программу , удаляющую из текста все слова содержащие абв
 
 a = 'абва ыва ыва  абв fsвабв ывабва фабвыв'.split()
-b = [lambda i:i+10 for i in a if 'абв' in i]
 s = list(filter(lambda i:'абв' not in i,a ))
 print(a)
 print(s)
